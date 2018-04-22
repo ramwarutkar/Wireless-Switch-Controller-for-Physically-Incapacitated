a27b595fa32dab4cbbba0e638715efa0b0214377
ramwarutkar <31826300+ramwarutkar@users.noreply.github.com>
2018-04-22 16:51:15 +0530

    Add files via upload

---
+++ Wireless Switch Controller.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+Servo myservo;
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+char data;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  while (Serial) {
+    ; 
+  }
+  
+  mySerial.begin(9600);
+  myservo.attach(9);
+  myservo.write(90);
+  delay(100);
+}
+
+void loop() { 
+  if (mySerial.available()) 
+  {
+    data = mySerial.read();
+  }
+  switch(data)
+  {
+    case '1':myservo.write(60);
+            break;
+    case '2':myservo.write(120);
+            break;
+    default:myservo.write(90);  
+  }
+  delay(10);
+}
